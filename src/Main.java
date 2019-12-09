86b13d8c4da2c30d2ffbf28201adcb6e5f0ca8a6
johnsuh01 <johnsuh01@hotmail.com>
2019-12-09 10:32:36 -0500

    1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args){
+        String myVar = "Jen";
+
+        System.out.println("Hello World!");
+        System.out.println(myVar);
+        System.out.println("Hello and welcome, " + myVar);
+
+
+}
